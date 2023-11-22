dd7ddfcd0e568b31649bb3ef9865a6b35f86ac17
Chris Cantoni <chris.cantoni@gmail.com>
2023-11-21 22:08:26 -0600

    First couple steps of base mode completed.

--- src/components/PlantList/PlantList.js
+++ src/components/PlantList/PlantList.js
@@ -5,17 +5,30 @@ import { useDispatch, useSelector} from 'react-redux';
 function PlantList() {
     const dispatch = useDispatch();
 
-    const reduxState = useSelector(store => store);
+    const plantList = useSelector(store => store.plantList);
+    const getPlantList = () => {
+        dispatch({type: 'FETCH_PLANTS'})
+    }
+
+    const deletePlant = (i) => {
+        console.log('You clicked delete', i)
+        dispatch({type: 'DELETE_PLANT', payload: i})
+   
+    }
 
     useEffect(() => {
         console.log('component did mount');
         // dispatch an action to request the plantList from the API
+        getPlantList();
     }, []); 
 
     return (
         <div>
             <h3>This is the plant list</h3>
-            <pre>{JSON.stringify(reduxState)}</pre>
+            {plantList.map((plant, i) => {
+            return <li key={i}>{plant.name} of the kingdom {plant.kingdom} and clade {plant.clade} <button onClick={() => deletePlant(i)}>delete</button></li>
+            })}
+            <pre>{JSON.stringify(plantList)}</pre>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,82 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
 import App from './App';
 
 // this startingPlantArray should eventually be removed
-const startingPlantArray = [
-  { id: 1, name: 'Rose' },
-  { id: 2, name: 'Tulip' },
-  { id: 3, name: 'Oak' }
-];
+// const startingPlantArray = [
+//   { id: 1, name: 'Rose' },
+//   { id: 2, name: 'Tulip' },
+//   { id: 3, name: 'Oak' }
+// ];
 
-const plantList = (state = startingPlantArray, action) => {
+const plantList = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_PLANT':
-      return [ ...state, action.payload ]
+    case 'SET_PLANTS':
+      return action.payload;
     default:
       return state;
   }
 };
 
+function* fetchPlants() {
+  try {
+      const response = yield axios.get('/api/plant');
+      const action = { type: 'SET_PLANTS', payload: response.data}
+      yield put(action);
+  } catch (error) {
+    console.log('Error fetching plants', error);
+    alert('Something went wrong!');
+  }
+}
+
+function* addPlant(action) {
+  try {
+    yield axios.post('/api/plant', action.payload);
+    yield put({type: 'FETCH_PLANTS'})
+  } catch (error) {
+    console.log('Error adding plant', error)
+    alert('Something went wrong!');
+  }
+}
+
+function* deletePlant(action) {
+  try {
+    yield axios.delete(`/api/plant/${action.payload}`)
+    yield put({type: 'DELETE_PLANT'})
+
+  } catch (error) {
+    console.log('Error with delete', error)
+    alert('Something went wrong!')
+  }
+}
+
+
+
+function* rootSaga() {
+  yield takeEvery('FETCH_PLANTS', fetchPlants);
+  yield takeEvery('ADD_PLANT', addPlant);
+  yield takeEvery('DELETE_PLANT', deletePlant);
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
-  combineReducers({ plantList }),
+  combineReducers({ 
+    plantList 
+  }),
+  applyMiddleware(sagaMiddleware, logger),
 );
+
+
+
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
